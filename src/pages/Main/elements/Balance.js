8c9ce45e96fcf7ba960750b29a37662f37439ae2
Gabriel Torres <al2012.163.torres@gmail.com>
2021-10-25 03:44:58 -0300

    Refactor of balance component

--- src/pages/Main/elements/Balance.js
+++ src/pages/Main/elements/Balance.js
@@ -3,47 +3,18 @@ import EntryTable from "./EntryTable";
 import UserContext from "../../../contexts/UserContext";
 
 import styled from "styled-components";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default function Balance() {
+    const [entries, setEntries] = useState([]);
     const {user} = useContext(UserContext);
-
-    const entries = [
-        {
-            date: "30/11",
-            description: "Almoço mãe",
-            value: 39.9,
-            income: false
-        },
-        {
-            date: "27/11",
-            description: "Mercado",
-            value: 542.54,
-            income: false
-        },
-        {
-            date: "26/11",
-            description: "Compras churrasco",
-            value: 67.6,
-            income: false
-        },
-        {
-            date: "20/11",
-            description: "Empréstimo Maria",
-            value: 500,
-            income: true
-        },
-        {
-            date: "15/11",
-            description: "Salário",
-            value: 3000,
-            income: true
-        },
-    ]
     
     return (
         <Wrapper>
-            <EntryTable entries={entries} />
+            {entries.length ? 
+                <EntryTable entries={entries} />
+                : <p>Não há registros de entrada ou saída</p>
+            }
         </Wrapper>
     );
 }
@@ -55,4 +26,18 @@ const Wrapper = styled.div`
     margin: 0px 24px;
     padding: 0px 12px 10px 16px;
     background-color: #FFF;
+
+    p {
+        width: 100%;
+        height: 100%;
+        padding: 0px 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-family: Raleway;
+        font-size: 20px;
+        font-weight: 400;
+        color: #868686;
+    }
 `;
